baf5af7c26596de61f6d68d6f6054ec38aabb551
fulaytar <fulaytar2405@gmail.com>
2024-01-09 23:09:02 +0200

    задачки з лекції

--- js/avtocheck.js
+++ js/avtocheck.js
@@ -125,3 +125,125 @@ const Ann = {
   age: 23,
 }
 user.info.bind(Ann)(); //це нова функція */
+
+/* const fnB =(number)=> console.log(`This number is ${number}`)
+fnB(8); */
+
+
+/* const summa = (a, b) => console.log(a + b);
+summa(8, 12); */
+
+/* const add = (a, b, c) => a + b + c;
+console.log(add(5, 4, 9));
+ */
+
+
+/* function calc(a, b, callback) {
+  const result = callback(a, b);
+  console.log(result);
+}
+
+calc(2, 3, (x, y) => {
+  const arr = [];
+  arr.push(x, y);
+  return arr;
+});
+
+ */
+
+/* const numbers = [5, 10, 15, 20, 25];
+let total = 0;
+numbers.forEach(element => total += element);
+console.log(total);
+ */
+
+/* const logItems = (item) => item.forEach((element, index) => console.log(`${index + 1} - ${element}`));
+logItems(["Mango", "ananas", "babanas"]); */
+
+
+/* const printContactsInfo = ({ names, phones }) => {
+  const nameList = names.split(",");
+  const phoneList = phones.split(",");
+nameList.forEach((elem, index) => console.log(`${elem} -- ${phoneList[index]}`));
+}
+
+    printContactsInfo({
+      names: "Мар'янка,Іванко,Дідо,Баба",
+      phones: "987,453,111,321"
+    }); */
+
+
+
+
+/* const calculateAverage = (...args) => {
+  let total = 0;
+  args.forEach(elem => total += elem);
+  return total / args.length;
+  
+};
+
+console.log(calculateAverage(1, 2, 3, 4));
+console.log(calculateAverage(14, 8, 2));
+console.log(calculateAverage(27, 43, 2, 8,36)); */
+
+
+
+/**
+ * Array.prototype.map()
+ * - Поелементо перебирає оригінальний масив
+ * - Не змінює оригінальний масив
+ * - Повертає новий масив такої ж довжини
+ */
+
+/* const allCars = [
+  { make: "Honda", model: "CR-V", amount: 14, price: 24045 },
+  { make: "Honda", model: "Accord", amount: 2, price: 22455 },
+  { make: "Mazda", model: "Mazda 6", amount: 8, price: 24195 },
+  { make: "Mazda", model: "CX-9", amount: 7, price: 31520 },
+  { make: "Toyota", model: "4Runner", amount: 19, price: 34210 },
+  { make: "Toyota", model: "Sequoia", amount: 16, price: 45560 },
+  { make: "Toyota", model: "Tacoma", amount: 4, price: 24320 },
+  { make: "Ford", model: "F-150", amount: 11, price: 27110 },
+  { make: "Ford", model: "Fusion", amount: 13, price: 22120 },
+  { make: "Ford", model: "Explorer", amount: 6, price: 31660 },
+]; */
+/**
+ * Нехай функція makeCarsWithDiscount повертає новий масив об'єктів із змінним
+ * значенням властивості price залежно від переданої знижки.
+ */
+
+/* const makeCarsWithDiscount = (cars, discount) => {
+  return cars.map(car => {
+    return {
+      ...car,
+      price: car.price - (car.price * discount),
+    }
+  })
+}
+
+console.table(makeCarsWithDiscount(allCars, 0.2));
+console.table(makeCarsWithDiscount(allCars, 0.4)); */
+
+
+/* const players = [
+  { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+  { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: true },
+  { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
+  { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
+  { id: "player-5", name: "Chelsy", timePlayed: 80, points: 48, online: true },
+];
+console.table(players);
+ */
+/*
+ * Збільшуємо кількість годин гравця за id
+ */
+
+/* const playerIdToUpdate = "player-3";
+const updatedPlayers = players.map((player) => {
+  if (player.id === playerIdToUpdate) {
+    player.timePlayed *=2;
+  }
+  return player;
+})
+
+console.table(updatedPlayers); */
