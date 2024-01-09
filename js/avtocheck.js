3ca4f0ea9d8c4117c810976fc523389fb709599c
fulaytar <fulaytar2405@gmail.com>
2024-01-09 23:12:58 +0200

    перевірив лекцію задачі

--- js/avtocheck.js
+++ js/avtocheck.js
@@ -247,3 +247,19 @@ const updatedPlayers = players.map((player) => {
 })
 
 console.table(updatedPlayers); */
+
+
+/* const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+
+tweets.forEach(
+  ({id, likes}) => console.log(id, likes)
+)
+const tags = tweets.flatMap(t => t.tags);
+console.log(tags); */
