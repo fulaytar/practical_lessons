b7330e8a969d8b7641539ca64f93cfc507acd397
fulaytar <fulaytar2405@gmail.com>
2024-03-02 17:16:00 +0200

    okey

--- js/load_news.js
+++ js/load_news.js
@@ -15,7 +15,8 @@ const searchForm = document.querySelector(".search-form");
 const articlesContainer = document.querySelector(".articles");
 const loadMoreBtn = document.querySelector('[data-action="load-more"]');
 const spinner = document.querySelector('[data-action="load-more"] span.spinner-border');
-
+let current_page;
+let current_query;
 const TOKEN = "ed8367e7875c46529ee602db3506e8ce";
 
 async function getNews(query, amount, page) {
@@ -47,13 +48,27 @@ function appendArticlesMarkup(articles) {
 }
 
 
+
+
 searchForm.addEventListener('submit', event => {
-    event.preventDefault();
+  event.preventDefault();
+  articlesContainer.innerHTML = null;
     console.log(event);
     const query = event.target.elements.query.value;
 
     getNews(query, 5, 1).then(data => {
-        console.log(data)
-        appendArticlesMarkup(data.articles)
+      appendArticlesMarkup(data.articles)
+      current_query = query;
+      current_page = 1;
+    })
+})
+
+loadMoreBtn.addEventListener('click', event => {
+  spinner.classList.remove('is-hidden')
+  current_page++;
+  getNews(current_query ,5, current_page).then(data => {
+    appendArticlesMarkup(data.articles)
+    spinner.scrollIntoView();
+    spinner.classList.add('is-hidden')
     })
 })
